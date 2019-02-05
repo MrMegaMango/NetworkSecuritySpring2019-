b000be58fe2fa76a117efcb4ede1fe8b8fa86710
MrMegaMango <wangzuo95911@gmail.com>
2019-02-04 20:53:22 -0500

    Update escape_room_server.py

--- src/exercises/ex2/escape_room_server.py
+++ src/exercises/ex2/escape_room_server.py
@@ -3,10 +3,22 @@ from escape_room import EscapeRoom
 server_socket.listen(1)
 while True:
     conn, addr = server_socket.accept()
-    
-    # create an escape room
-    # while the escape room status is locked
-        # read the data from conn
+    room=EscapeRoom()      # create an escape room
+    room.start()
+    while room.status()=="locked":      # while the escape room status is locked
+        #command = input (">> ")
+
+        data = conn.recv(1024)              # read the data from conn
+
         output = room.command(data.decode()) # encode converts from bytes to string
-        # send the output.encode() to conn (encode converts from string to bytes)
+        
+        conn.sendall(data)               # send the output.encode() to conn (encode converts from string to bytes)
+        
+#         if room.status()=="escaped":
+
+#             return "Congratulations! You escaped!"
+
+#         else:
+
+#             return "Oh no! The clock starts ringing!!! After a few seconds, the room fills with a deadly gas... Sorry. You died."
    conn.close()
