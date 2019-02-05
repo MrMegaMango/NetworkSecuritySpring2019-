3efecc8b58ee005549e9c3344c36c8b7aa1fc976
MrMegaMango <wangzuo95911@gmail.com>
2019-02-04 20:57:33 -0500

    Update escape_room_client.py

--- src/exercises/ex2/escape_room_client.py
+++ src/exercises/ex2/escape_room_client.py
@@ -1,1 +1,10 @@
+import socket
 
+HOST = 'daring.cwi.nl'
+PORT = 50007
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+  s.connect((HOST, PORT))
+  s.endall(b'Hello, world')
+  data = s.recv(1024)
+  
+print('Received',repr(data))
