c68bebfe6a7b31e654e78d966730fa95881bd0d4
MrMegaMango <wangzuo95911@gmail.com>
2019-02-05 23:31:04 -0500

    Update escape_room_server.py
    Server works. Just some format clean up. Server shuts down if invalid input is given.

--- src/exercises/ex2/escape_room_server.py
+++ src/exercises/ex2/escape_room_server.py
@@ -1,51 +1,22 @@
 from escape_room import EscapeRoom
 import socket
-
-
-#HOST='73.39.109.232'
 HOST='127.0.0.1'
-PORT=1000
-
-
-
-
-#server_socket.listen(1)
+PORT=10054
 s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, PORT))
 s.listen(1)
 while True:
-	print("0")
-	#s.connect(('73.39.109.232',50911))
-	print("1")
 	conn, addr = s.accept()
-	print(conn)
-	print("2")
 	room=EscapeRoom()      # create an escape room
 	room.start()
 	while room.status()=="locked":      # while the escape room status is locked
-	#command = input (">> ")
-		print("ready to receive")
 		data = conn.recv(1024)              # read the data from conn
-		print(data)
-		print(data.decode())
-		print(data.decode())
-		print(data.decode())
-
-		print(type(data.decode()))
 		datastr=data.decode()
 		datastr=datastr.replace('\r\n','')
-		print(datastr)
 		output = room.command(datastr) # encode converts from bytes to string
-		print(output)
-		print(type(output))
-		
 		conn.sendall(output.encode())               # send the output.encode() to conn (encode converts from string to bytes)
-
-	#         if room.status()=="escaped":
-
-	#             return "Congratulations! You escaped!"
-
-	#         else:
-
-	#             return "Oh no! The clock starts ringing!!! After a few seconds, the room fills with a deadly gas... Sorry. You died."
+	if room.status()=="escaped":
+		conn.sendall("Congratulations! You escaped!".encode())
+	else:
+		conn.sendall("Oh no! The clock starts ringing!!! After a few seconds, the room fills with a deadly gas... Sorry. You died.".encode())
 	conn.close()
