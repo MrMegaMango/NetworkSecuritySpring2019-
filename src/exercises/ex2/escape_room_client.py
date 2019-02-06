16d4abd85b82725ad1bb2685376b61d99ec88db4
MrMegaMango <wangzuo95911@gmail.com>
2019-02-06 15:38:07 -0500

    Update escape_room_client.py

--- src/exercises/ex2/escape_room_client.py
+++ src/exercises/ex2/escape_room_client.py
@@ -2,13 +2,8 @@ import socket
 HOST = '127.0.0.1'
 PORT = 50054
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-  s.bind((HOST, PORT))
-  l='look'
-  b=b'look'
-  print(b)
-  print(type(b))
-  s.send(b'look')
-  print("ok?")
+  s.connect((HOST, PORT))
+  s.sendall(b'look')
   data = s.recv(1024)
   
 print('Received',repr(data))
