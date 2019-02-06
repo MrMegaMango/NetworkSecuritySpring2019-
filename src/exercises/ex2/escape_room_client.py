37f3dba99a11adb78463a109b6a01bc1108001c2
MrMegaMango <wangzuo95911@gmail.com>
2019-02-06 00:26:51 -0500

    broken pipe

--- src/exercises/ex2/escape_room_client.py
+++ src/exercises/ex2/escape_room_client.py
@@ -1,10 +1,14 @@
 import socket
-
-HOST = 'daring.cwi.nl'
-PORT = 50007
+HOST = '127.0.0.1'
+PORT = 50054
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-  s.connect((HOST, PORT))
-  s.endall(b'Hello, world')
+  s.bind((HOST, PORT))
+  l='look'
+  b=b'look'
+  print(b)
+  print(type(b))
+  s.send(b'look')
+  print("ok?")
   data = s.recv(1024)
   
 print('Received',repr(data))
