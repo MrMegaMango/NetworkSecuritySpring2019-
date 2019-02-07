dec975a47d2a798f401aafab6a701444f9c439aa
MrMegaMango <wangzuo95911@gmail.com>
2019-02-06 23:28:08 -0500

    implemented both extra credit parts
    Took me a while to figure out the while room.status == "locked" is preventing any other client connecting if client 1 is playing, problem is fixed by changing while loop to if statement.

--- src/exercises/ex2/escape_room_server.py
+++ src/exercises/ex2/escape_room_server.py
@@ -3,47 +3,41 @@ import select
 import socket
 import argparse
 def main(PORT):
-	HOST='0.0.0.0'
-	#PORT=1117
+	HOST='localhost'
 	s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 	room=[]
 	i=-1
-
 	s.bind((HOST, PORT))
 	inputs=[s]
-
-
-	s.listen(1)
+	s.listen(5)
 	while True:
 		read_ready, _, _ = select.select(inputs,[],[])
 		for server in read_ready:
-			if s == server:
-				
+			if s == server:			
 				conn, addr = server.accept()
-				inputs.append(conn)			
-
+				inputs.append(conn)	
 			else:
 				room.append(EscapeRoom())
 				i=i+1     # create an escape room
 				room[i].start()
-				while room[i].status()=="locked":      # while the escape room status is locked
+				if room[i].status()=="locked":      # while the escape room status is locked
 					data = server.recv(1024)              # read the data from conn
 					datastr=data.decode()
 					datastr=datastr.replace('\r\n','')
 					output = room[i].command(datastr) # encode converts from bytes to string
-					if output:
-						server.sendall(output.encode())               # send the output.encode() to conn (encode converts from string to bytes)
-					else:
-						server.sendall("invalid input".encode())
+					print(output)
+					try:					
+						if output:
+							server.sendall(output.encode())               # send the output.encode() to conn (encode converts from string to bytes)
+						else:
+							server.sendall("invalid input".encode())
+					except BrokenPipeError:	
+						break	
 				if room[i].status()=="escaped":
 					server.sendall("Congratulations! You escaped!".encode())
-				else:
+				elif room[i].status()=="died":
 					server.sendall("Oh no! The clock starts ringing!!! After a few seconds, the room fills with a deadly gas... Sorry. You died.".encode())
-				server.close()
 				break
-
-
 if __name__=="__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--port",help="the port you choose")
@@ -53,4 +47,4 @@ if __name__=="__main__":
         PORT=int(args.port)
     else:
         PORT=1121
-    main(PORT)
+    main(PORT).serve()
