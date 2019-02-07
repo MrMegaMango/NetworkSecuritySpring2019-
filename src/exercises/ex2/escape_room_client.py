60c2c980bdb1a53914b720c80a9a195b8654a091
MrMegaMango <wangzuo95911@gmail.com>
2019-02-06 20:03:02 -0500

    working client without select

--- src/exercises/ex2/escape_room_client.py
+++ src/exercises/ex2/escape_room_client.py
@@ -1,9 +1,13 @@
 import socket
-HOST = '127.0.0.1'
-PORT = 50054
+HOST = '0.0.0.0'
+PORT = 11113
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-  s.connect((HOST, PORT))
-  s.sendall(b'look')
-  data = s.recv(1024)
-  
-print('Received',repr(data))
+	s.connect((HOST, PORT))
+	while True:
+		command = input (">> ")
+		s.send(command.encode())
+		data = s.recv(1024)
+		data = data.decode()
+		print(data)
+		if data == "You open the door.Congratulations! You escaped!" or data == "Oh no! The clock starts ringing!!! After a few seconds, the room fills with a deadly gas... Sorry. You died.":
+			break
