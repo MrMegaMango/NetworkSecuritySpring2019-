e953240926980dac17d92040497d1218c4730059
MrMegaMango <wangzuo95911@gmail.com>
2019-02-06 23:29:32 -0500

    final check before deadline
    make sure everything runs smoothly

--- src/exercises/ex2/escape_room_client.py
+++ src/exercises/ex2/escape_room_client.py
@@ -2,35 +2,18 @@ import select
 import socket
 import sys
 import argparse
-
 def main(PORT):
 	HOST = '0.0.0.0'
-	#PORT = 1121
-
-
-
 	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 		s.connect((HOST,PORT))
 		while True:
-
-					
-
-
 			command = input (">> ")
-
 			s.send(command.encode())
-
 			data = s.recv(1024)
 			data = data.decode()
-			
-
 			print(data)
-			if data == "You open the door.Congratulations! You escaped!" or data == "Oh no! The clock starts ringing!!! After a few seconds, the room fills with a deadly gas... Sorry. You died.":
-				
+			if data == "You open the door.Congratulations! You escaped!" or data == "Oh no! The clock starts ringing!!! After a few seconds, the room fills with a deadly gas... Sorry. You died.":				
 				break
-
-
-
 if __name__=="__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--port",help="the port you choose")
