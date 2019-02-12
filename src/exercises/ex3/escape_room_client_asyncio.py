5f630935cb386014e736664e452b8fdaa8060220
MrMegaMango <wangzuo95911@gmail.com>
2019-02-11 16:47:51 -0800

    asyncio working

--- src/exercises/ex3/escape_room_client_asyncio.py
+++ src/exercises/ex3/escape_room_client_asyncio.py
@@ -1,37 +1,19 @@
-import select
-import socket
-import sys
 import argparse
 import asyncio
-import time
 class EscapeRoomClientProtocol(asyncio.Protocol):
-	def __init__(self,message):
-		message = ""
-		self.message=message
-		#global PORT
-		#HOST = '0.0.0.0'
-			#print("init")
-		#with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-			#s.connect((HOST,PORT))
-
-			#command = input (">> ")
-				
-
-
-
 
 	def connection_made(self,transport):
-		time.sleep(1)
-		message = input (">> ")
-		print("stop")
-		self.message=message
-		transport.write(self.message.encode())
+		command= input(">> ")
+		self.command=command
+		self.transport=transport
+		transport.write(self.command.encode())
+
 	def data_received(self,data):
 		print(data.decode())
-		
+		command = input (">> ")
+		self.transport.write(command.encode())
 
 if __name__=="__main__":
-    #global PORT
     parser = argparse.ArgumentParser()
     parser.add_argument("--port",help="the port you choose")
     args=parser.parse_args()
@@ -40,12 +22,8 @@ if __name__=="__main__":
         PORT=int(args.port)
     else:
         PORT=1121
-    #main(PORT)
     loop=asyncio.get_event_loop()
-    message = "hi88"
-    coro=loop.create_connection(lambda: EscapeRoomClientProtocol(message),'0.0.0.0',PORT)
-    print("hi")
+    coro=loop.create_connection(lambda: EscapeRoomClientProtocol(),'0.0.0.0',PORT)
     loop.run_until_complete(coro)
-    print("hi2")
     loop.run_forever()
     looop.close()
