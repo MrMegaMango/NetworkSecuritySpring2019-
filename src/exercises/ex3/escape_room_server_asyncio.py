0ac24c3d2bdbd7291eb447fb4f80c60a145f159c
MrMegaMango <wangzuo95911@gmail.com>
2019-02-11 18:12:15 -0800

    asyncio and extra credit
    every game uses different room

--- src/exercises/ex3/escape_room_server_asyncio.py
+++ src/exercises/ex3/escape_room_server_asyncio.py
@@ -5,16 +5,17 @@ import asyncio
 class EscapeRoomServerClientProtocol(asyncio.Protocol):
     def connection_made(self,transport):
         global room
+        room=[]
         self.transport=transport
-        room=EscapeRoom()
-        room.start()
+        room.append(EscapeRoom())
+        room[-1].start()
     def data_received(self,data):
         global room
         datastr=data.decode()
         datastr=datastr.replace('\r\n','')
         datastr=datastr.replace('\n','')
-        if room.status()=="locked":
-            output = room.command(datastr) # encode converts from bytes to string
+        if room[-1].status()=="locked":
+            output = room[-1].command(datastr) # encode converts from bytes to string
         try:
             if output:
                 self.transport.write(output.encode())               # send the output.encode() to conn (encode converts from string to bytes)
@@ -22,10 +23,10 @@ class EscapeRoomServerClientProtocol(asyncio.Protocol):
                 self.transport.write("invalid input".encode())
         except BrokenPipeError:
             print("pipe")
-        if room.status()=="escaped":
+        if room[-1].status()=="escaped":
             self.transport.write("Congratulations! You escaped!".encode())
 
-        elif room.status()=="died":
+        elif room[-1].status()=="died":
             self.transport.write("Oh no! The clock starts ringing!!! After a few seconds, the room fills with a deadly gas... Sorry. You died.".encode())
 parser = argparse.ArgumentParser()
 parser.add_argument("--port",help="the port you choose")
