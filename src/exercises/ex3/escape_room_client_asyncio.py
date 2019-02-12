be8939324e9740454d7589de1d922fc0be3c9ae0
MrMegaMango <wangzuo95911@gmail.com>
2019-02-11 17:57:53 -0800

    extra credit, used queue.get instead of input
    to better handle async user input

--- src/exercises/ex3/escape_room_client_asyncio.py
+++ src/exercises/ex3/escape_room_client_asyncio.py
@@ -1,18 +1,21 @@
 import argparse
 import asyncio
+import sys
+def got_stdin_data(q):
+    asyncio.async(q.put(sys.stdin.readline()))
 class EscapeRoomClientProtocol(asyncio.Protocol):
-
 	def connection_made(self,transport):
-		command= input(">> ")
-		self.command=command
+		print(">> ",end='',flush=True)
+		command=asyncio.async(q.get())
+		command.add_done_callback(self.write_reply)
 		self.transport=transport
-		transport.write(self.command.encode())
-
 	def data_received(self,data):
-		print(data.decode())
-		command = input (">> ")
-		self.transport.write(command.encode())
-
+		print(data.decode()+"\n>> ",end='')
+		command=asyncio.async(q.get())
+		command.add_done_callback(self.write_reply)
+	def write_reply(self,command):
+		reply = command.result()
+		self.transport.write(reply.encode())
 if __name__=="__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--port",help="the port you choose")
@@ -23,6 +26,8 @@ if __name__=="__main__":
     else:
         PORT=1121
     loop=asyncio.get_event_loop()
+    q=asyncio.Queue()
+    loop.add_reader(sys.stdin,got_stdin_data,q)
     coro=loop.create_connection(lambda: EscapeRoomClientProtocol(),'0.0.0.0',PORT)
     loop.run_until_complete(coro)
     loop.run_forever()
