bda53a7654c06371c8670cbeb0c93186b9a9f2f9
MrMegaMango <wangzuo95911@gmail.com>
2019-02-13 19:51:07 -0800

    with proper await response

--- src/exercises/ex4/escape_room_client_asyncio2.py
+++ src/exercises/ex4/escape_room_client_asyncio2.py
@@ -3,25 +3,32 @@ import asyncio
 import sys
 import time
 stdin_queue=[]
+stdout_queue=[]
 def handle_stdin():
     line_in=sys.stdin.readline()
     line_in=line_in[:-1]
     stdin_queue.append(line_in)
-async def async_input(prompt):
-    print(prompt,end="",flush=True)
+async def async_input():
+    print(">> ",end="",flush=True)
     while len(stdin_queue)==0:     
         await asyncio.sleep(.1)   
     return stdin_queue.pop(0)
+async def async_output():
+    while len(stdout_queue)==0:     
+        await asyncio.sleep(.1)   
+    return stdout_queue.pop(0)
 async def game_runner(protocol):
     while True:
-        command=await async_input(">> ")    
+        command=await async_input()    
         protocol.transport.write(command.encode())
+        result=await async_output()
+        print(result)
 class EscapeRoomClientProtocol(asyncio.Protocol):
 	def connection_made(self,transport):
 		print("connection made")
 		self.transport=transport
 	def data_received(self,data):
-		print(data.decode()+"\n>> ",end='')
+		stdout_queue.append(data.decode())
 if __name__=="__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--port",help="the port you choose")
